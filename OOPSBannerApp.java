fcd6651dec74674fb75c5712ec108f8042434bce
vivekdesai1215 <temporarygithub1205@gmail.com>
2026-02-18 15:18:34 +0530

    UC4 : Coded to print OS using Array ForEach Loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,11 +3,32 @@
 
 
 public class OOPSBannerApp{
+
 	public static void main(String[] args){
-		System.out.println(String.join(" ,","  ***  ","  *****"));
-		System.out.println(String.join(" ,"," ** ** ","  **   "));
-		System.out.println(String.join(" ,","**   **","  *****"));
-		System.out.println(String.join(" ,","**   **","     **"));
-		System.out.println(String.join(" ,","  ***  ","  *****"));
-	}
+
+		String[] arr = new String[7];
+
+		arr[0] = String.join(" ", "   ****   ", "   *****   ");
+
+		arr[1] = String.join(" ", " **    ** ", "   **      ");
+
+		arr[2] = String.join(" ", " **    ** ", "   **      ");
+
+		arr[3] = String.join(" ", " **    ** ", "   *****   ");
+
+		arr[4] = String.join(" ", " **    ** ", "      **   ");
+
+		arr[5] = String.join(" ", " **    ** ", "      **   ");
+
+		arr[6] = String.join(" ", "   ****   ", "   *****   ");
+
+		for(String s : arr){
+
+			System.out.println(s);
+
+		}
+
+	}	
+
 }
+ 
