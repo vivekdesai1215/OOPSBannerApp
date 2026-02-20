ac092f581dda4a44fd8402c4981ad7349e6097d2
vivekdesai1215 <temporarygithub1205@gmail.com>
2026-02-20 10:24:39 +0530

    UC7 : Coded to print OOPS using Inner class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,24 +1,78 @@
-//@author Developer
-//@version 6
 
 
 public class OOPSBannerApp{
 	public static void main(String[] args){
-
-		String[] pattern = getPattern();
-		for(String s: pattern){
-			System.out.println(s);
-		}
-	};	
-	public static String[] getPattern(){
-		return new String[]{
-			"  ***  ",
-			" ** ** ",
-			"**   **",
-			"**   **",
-			" ** ** ",
-			"  ***  "
+		CharacterPatternMap[] charMaps = createCharacterPatternMap();
+		String message = "OOPS";
+		printMessage(message,charMaps);
+		
+	}
+	static class CharacterPatternMap{
+		Character character;
+		String[] pattern;
+		
+		public CharacterPatternMap(char ch, String[] pattern){
+			this.character = ch;
+			this.pattern = pattern;
+		}	
+	}
+	
+	public static CharacterPatternMap[] createCharacterPatternMap(){
+		char[] chars = {'O','O','P','S'};
+		String[][] patterns = {
+			{
+				"  ***  ",
+				" ** ** ",
+				"**   **",
+				"**   **",
+				" ** ** ",
+				"  ***  "
+			},
+			{
+				"  ***  ",
+				" ** ** ",
+				"**   **",
+				"**   **",
+				" ** ** ",
+				"  ***  "
+			},
+			{
+				"*******",
+				"**   **",
+				"**   **",
+				"****** ",
+				"**     ",
+				"**     "
+			},
+			{
+				"*******",
+				"**     ",
+				"**     ",
+				"*******",
+				"     **",
+				"*******"
+			}
 		};
-	};
+		
+		CharacterPatternMap[] cp = new CharacterPatternMap[4];
+		for(int i=0;i<4;i++){
+			cp[i] = new CharacterPatternMap(chars[i],patterns[i]);
+		}
+		
+		return cp;
+	}
+	
+	
+	public static void printMessage(String message,CharacterPatternMap[] charMaps){
+		if(message.equals("OOPS")){
+			for(CharacterPatternMap cp : charMaps){
+				String[] str = cp.pattern;
+				
+				for(String s : str){
+					System.out.println(s );
+				}
+			}
+		}
+	}
 }
  
